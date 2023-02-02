c3d5c80083c87e2e75611bccef26e809702b9d09
Boris Konak <boriskonak01@gmail.com>
2023-02-02 16:27:44 +0200

    mapped the actors on the actor page

--- client/src/view/ActorsPage.js
+++ client/src/view/ActorsPage.js
@@ -1,3 +1,5 @@
+import { Card, CardContent, CardMedia, Typography } from "@mui/material";
+import { Box } from "@mui/system";
 import React, { useEffect, useState } from "react";
 
 const ActorsPage = () => {
@@ -6,18 +8,35 @@ const ActorsPage = () => {
     const test1 = await fetch("http://127.0.0.1:8000/api/actors");
     const test2 = await test1.json();
     setActor(test2);
-    useEffect(() => {
-      FetchActor();
-    }, []);
+    console.log(actor);
   };
+  useEffect(() => {
+    FetchActor();
+  }, []);
   return (
     <>
       {actor.map((person) => {
-        return <h1>{`${person.name.first_name} ${person.name.last_name}`}</h1>;
+        return (
+          <Card sx={{ display: `flex` }}>
+            <Box sx={{ display: "flex", flexDirection: "column" }}>
+              <CardContent sx={{ flex: "1 0 auto" }}>
+                <Typography>{`${person.name.first_name} ${person.name.last_name}`}</Typography>
+                <Typography>{`${person.dob.date}`}</Typography>
+                <Typography>{`${person.dob.location}`}</Typography>
+                <Typography>{`${person.biography}`}</Typography>
+              </CardContent>
+            </Box>
+            <CardMedia
+              component="img"
+              sx={{ width: 151 }}
+              image={person.image}
+              alt=""
+            />
+          </Card>
+        );
       })}
     </>
   );
 };
-
 
 export default ActorsPage;
