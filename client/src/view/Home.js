ab5c0d4936b96302a030488cb6fc6516d32871d5
Boris Konak <boriskonak01@gmail.com>
2023-02-03 19:17:03 +0200

    actor birthdays

--- client/src/view/Home.js
+++ client/src/view/Home.js
@@ -43,9 +43,7 @@ const Home = () => {
   const ratearr = top.filter((i) => i.rating.count >= 750000);
 
   const current = new Date();
-  const date = `${current.getDate()}/${
-    current.getMonth() + 1
-  }/${current.getFullYear()}`;
+  const date = `${current.getDate()}/${current.getMonth() + 1}`;
   const bdayarr = actor.filter((i) => i.dob.date === date);
   return (
     <div className="enlarge">
