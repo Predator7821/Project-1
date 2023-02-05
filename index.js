ec11968b7f60ff87a29ba51ed4c3bfc50921345a
Boris Konak <boriskonak01@gmail.com>
2023-02-05 20:03:40 +0200

    dynamic route

--- index.js
+++ index.js
@@ -121,18 +121,18 @@ app.get("/api/movies", async (req, res) => {
   }
 });
 
-app.get("/api/movies/:movieid", async (req,res) => {
-  try{
-    const movie = await Movie.findOne({_id: req.params.movieid})
-    if(!movie){
-      res.status(404).send({message:"no such movie in the DB"})
+app.get("/api/movies/:movieid", async (req, res) => {
+  try {
+    const movie = await Movie.findOne({ id: req.params.movieid });
+    if (!movie) {
+      res.status(404).send({ message: "no such movie in the DB" });
     }
-    res.send(movie)
-  } catch(e){
-    console.log(e)
-    res.status(500).send({message:e})
+    res.send(movie);
+  } catch (e) {
+    console.log(e);
+    res.status(500).send({ message: e });
   }
-})
+});
 
 const GetUser = new mongoose.Schema({
   Username: {
