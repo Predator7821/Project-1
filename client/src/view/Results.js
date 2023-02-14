61f76b00f9ffb7a6e5d0aaecb4709ce92a32413f
Boris Konak <boriskonak01@gmail.com>
2023-02-14 17:47:07 +0200

    search working now

--- client/src/view/Results.js
+++ client/src/view/Results.js
@@ -1,13 +1,15 @@
+import { CardContent, Card, Typography, CardMedia } from "@mui/material";
 import React, { useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { Catsearchcontext, Searchresultscontext } from "../context/Passdata";
 import "./Results.css";
 const Results = () => {
   const { results, setResults } = useContext(Searchresultscontext);
-  const { searchCat, setSearchCat }=useContext(Catsearchcontext);
-  let allmap=(false)
-  let moviemap=(false)
-  let actormap=(false)
-  let usermap=(false)
+  const { searchCat, setSearchCat } = useContext(Catsearchcontext);
+  let allmap = false;
+  let moviemap = false;
+  let actormap = false;
+  let usermap = false;
 
   const [actor, setActor] = useState([]);
   const [movie, setMovie] = useState([]);
@@ -15,12 +17,12 @@ const Results = () => {
   const getmovies = async () => {
     fetch("http://127.0.0.1:8000/api/movies")
       .then((response) => response.json())
-      .then((data) => setActor(data));
+      .then((data) => setMovie(data));
   };
   const getactor = async () => {
     fetch("http://127.0.0.1:8000/api/actors")
       .then((response) => response.json())
-      .then((data) => setMovie(data));
+      .then((data) => setActor(data));
   };
   const getusers = async () => {
     fetch("http://127.0.0.1:8000/api/users")
@@ -28,39 +30,173 @@ const Results = () => {
       .then((data) => setUser(data));
   };
   if (searchCat === "All") {
-      getmovies()
-      getactor()
-      getusers()
-      allmap=true
-  }else if(searchCat==="Actors"){
-    getactor()
-    actormap=true
-  }else if(searchCat==="Movies"){
-    getmovies()
-    moviemap=true
-  }else{
-    getusers()
-    usermap=true
+    getmovies();
+    getactor();
+    getusers();
+    allmap = true;
+  } else if (searchCat === "Actors") {
+    getactor();
+    actormap = true;
+  } else if (searchCat === "Movies") {
+    getmovies();
+    moviemap = true;
+  } else {
+    getusers();
+    usermap = true;
   }
-  const allmovies = movie.filter((e) => e.name === results);
-  const allusers = user.filter((e)=> e.Username === results)
-  const allactors = actor.filter((e)=> e.name === results)
-  return(
-    <div className="thatfuckingheader makeitfitthecenter">
-      {allmap ? 
-    (allmovies.map((e)=> {
-      return(
-        <h1>{e.name}</h1>
-      )
-    })) +(allusers.map((e)=>{
-      return(
-        <h1>{e.Username}</h1>
-      )
-    }))+ (allactors.map((e)=>{
-      return(
-        <h1>{e.name}</h1>
-      )
-    })):<div></div>}
+  const allmovies = movie.filter(
+    (e) => e.name.toLowerCase() === results.toLowerCase()
+  );
+  const allusers = user.filter(
+    (e) => e.Username.toLowerCase() === results.toLowerCase()
+  );
+  const allactors = actor.filter(
+    (e) => e.name.first_name.toLowerCase() === results.toLowerCase()
+  );
+  return (
+    <div className="thatfuckingheader makeitfitthecenter organizedcards">
+      {allmap ? (
+        allmovies.map((e) => {
+          return (
+            <Card sx={{ minWidth: 345, maxWidth: 345, margin: 5 }}>
+              <Link to={`/movies/${e._id}`}>
+                <CardMedia
+                  component="img"
+                  alt="green iguana"
+                  height="500"
+                  image={e.picture}
+                />
+              </Link>
+
+              <CardContent className="makeitlookbetter">
+                <Typography>{e.name}</Typography>
+                <Typography>{e.category}</Typography>
+                <Typography>{e.type}</Typography>
+                <Typography>{e.date}</Typography>
+              </CardContent>
+            </Card>
+          );
+        })
+      ) : (
+        <div></div>
+      )}
+      {allmap ? (
+        allusers.map((e) => {
+          return (
+            <Card sx={{ minWidth: 345, maxWidth: 345, margin: 5 }}>
+              <Link to={`/users/${e._id}`}>
+                <CardMedia
+                  component="img"
+                  alt="green iguana"
+                  height="500"
+                  image={e.picture}
+                />
+              </Link>
+
+              <CardContent className="makeitlookbetter">
+                <Typography>{e.pfp}</Typography>
+              </CardContent>
+            </Card>
+          );
+        })
+      ) : (
+        <div></div>
+      )}
+      {allmap ? (
+        allactors.map((e) => {
+          return (
+            <Card sx={{ minWidth: 345, maxWidth: 345, margin: 5 }}>
+              <Link to={`/actors/${e._id}`}>
+                <CardMedia
+                  component="img"
+                  alt="green iguana"
+                  height="500"
+                  image={e.picture}
+                />
+              </Link>
+
+              <CardContent className="makeitlookbetter">
+                <Typography>
+                  {e.name.first_name} {e.name.last_name}
+                </Typography>
+              </CardContent>
+            </Card>
+          );
+        })
+      ) : (
+        <div></div>
+      )}
+      {usermap ? (
+        allusers.map((e) => {
+          return (
+            <Card sx={{ minWidth: 345, maxWidth: 345, margin: 5 }}>
+              <Link to={`/users/${e._id}`}>
+                <CardMedia
+                  component="img"
+                  alt="green iguana"
+                  height="500"
+                  image={e.picture}
+                />
+              </Link>
+
+              <CardContent className="makeitlookbetter">
+                <Typography>{e.pfp}</Typography>
+              </CardContent>
+            </Card>
+          );
+        })
+      ) : (
+        <div></div>
+      )}
+      {moviemap ? (
+        allmovies.map((e) => {
+          return (
+            <Card sx={{ minWidth: 345, maxWidth: 345, margin: 5 }}>
+              <Link to={`/movies/${e._id}`}>
+                <CardMedia
+                  component="img"
+                  alt="green iguana"
+                  height="500"
+                  image={e.picture}
+                />
+              </Link>
+
+              <CardContent className="makeitlookbetter">
+                <Typography>{e.name}</Typography>
+                <Typography>{e.category}</Typography>
+                <Typography>{e.type}</Typography>
+                <Typography>{e.date}</Typography>
+              </CardContent>
+            </Card>
+          );
+        })
+      ) : (
+        <div></div>
+      )}
+      {actormap ? (
+        allactors.map((e) => {
+          return (
+            <Card sx={{ minWidth: 345, maxWidth: 345, margin: 5 }}>
+              <Link to={`/actors/${e._id}`}>
+                <CardMedia
+                  component="img"
+                  alt="green iguana"
+                  height="500"
+                  image={e.picture}
+                />
+              </Link>
+
+              <CardContent className="makeitlookbetter">
+                <Typography>
+                  {e.name.first_name} {e.name.last_name}
+                </Typography>
+              </CardContent>
+            </Card>
+          );
+        })
+      ) : (
+        <div></div>
+      )}
     </div>
   );
 };
