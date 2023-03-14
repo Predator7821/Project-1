fe9923873a421c050c5319480f08e1416ef20214
Boris Konak <boriskonak01@gmail.com>
2023-03-14 21:05:35 +0200

    deleted /

--- client/src/constants/const.js
+++ client/src/constants/const.js
@@ -1,6 +1,6 @@
 export const SERVER_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:8000"
-    : "https://movie-app-project.onrender.com/";
+    : "https://movie-app-project.onrender.com";
 
 export const AMOUNT_OF_TIME_TO_STAY_LOGGED_IN_WITH_MS = 60 * 60 * 8 * 1000;
